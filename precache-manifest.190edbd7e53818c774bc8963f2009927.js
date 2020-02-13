4c27776498644c9b63bd4e5447a1214e62f8f193
Andrei Luca <aluca@psft.com>
2020-02-13 18:03:17 +0200

    Updates

---
+++ precache-manifest.190edbd7e53818c774bc8963f2009927.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147534285f05ff4f1f922e90e9e83929",
+    "url": "/crama-ferdi/index.html"
+  },
+  {
+    "revision": "880c9adf1d2c4ea79e57",
+    "url": "/crama-ferdi/static/css/main.215af82c.chunk.css"
+  },
+  {
+    "revision": "04a4378269dd812c8085",
+    "url": "/crama-ferdi/static/js/2.1f52dd7e.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/crama-ferdi/static/js/2.1f52dd7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880c9adf1d2c4ea79e57",
+    "url": "/crama-ferdi/static/js/main.45c9ddc8.chunk.js"
+  },
+  {
+    "revision": "5c98475de7c410a8ce03",
+    "url": "/crama-ferdi/static/js/runtime-main.58cdab93.js"
+  },
+  {
+    "revision": "c8f038803c691b3abff55bd3d6010806",
+    "url": "/crama-ferdi/static/media/1.c8f03880.jpeg"
+  },
+  {
+    "revision": "95bcc04bbe69983aa7de9e6ee9f7cb9d",
+    "url": "/crama-ferdi/static/media/2.95bcc04b.jpg"
+  },
+  {
+    "revision": "4043cde2aeac5b6dc1a12a966a68b736",
+    "url": "/crama-ferdi/static/media/3.4043cde2.jpg"
+  },
+  {
+    "revision": "a4117458940cb72e6f533af686e2e428",
+    "url": "/crama-ferdi/static/media/41.a4117458.jfif"
+  },
+  {
+    "revision": "ecd0b5e6374563c459a1f07d45042523",
+    "url": "/crama-ferdi/static/media/5.ecd0b5e6.webp"
+  },
+  {
+    "revision": "7a1fb9b0125a853e1a133bc2aa68cde9",
+    "url": "/crama-ferdi/static/media/6.7a1fb9b0.webp"
+  },
+  {
+    "revision": "ea7bb9cffcb121e768a314d92f882a2e",
+    "url": "/crama-ferdi/static/media/bg2.ea7bb9cf.jpg"
+  },
+  {
+    "revision": "f30991d3901ba69b2f7f53de40216a6a",
+    "url": "/crama-ferdi/static/media/call2.f30991d3.svg"
+  },
+  {
+    "revision": "b225d3c0e5d03012826a8c380f30035c",
+    "url": "/crama-ferdi/static/media/gmail.b225d3c0.svg"
+  },
+  {
+    "revision": "bdc54d17e8771ad49b52f923e13d304a",
+    "url": "/crama-ferdi/static/media/man.bdc54d17.jpg"
+  },
+  {
+    "revision": "560470a66f3f5685c01e5ef75b8bf7b8",
+    "url": "/crama-ferdi/static/media/profile1.560470a6.jpg"
+  },
+  {
+    "revision": "b90cd05b8b98b350017a74209ad7b5a5",
+    "url": "/crama-ferdi/static/media/up-ar.b90cd05b.svg"
+  },
+  {
+    "revision": "837d225a31b090684251d0fee08cea9e",
+    "url": "/crama-ferdi/static/media/wine2.837d225a.jpg"
+  },
+  {
+    "revision": "201bed53a81a28326265617041d150d7",
+    "url": "/crama-ferdi/static/media/wine3.201bed53.jpg"
+  },
+  {
+    "revision": "43ee187debf6208d2b3b79953665ae0f",
+    "url": "/crama-ferdi/static/media/wine4.43ee187d.jpg"
+  },
+  {
+    "revision": "df19d93e03e643f3a255cff5f6ed2ff0",
+    "url": "/crama-ferdi/static/media/wine6.df19d93e.jpg"
+  }
+]);
